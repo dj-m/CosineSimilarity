0359134a02be477f4d184eec96f43c6f24e38b8d
dj-m <domingomoronta@gmail.com>
2020-09-28 17:48:04 -0700

    Added data science cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Application of Cosine Similarity, calculated for a numeric dataset, while comparing the utility of it as a method for record matching and NLP projects.',
+    author='Domingo Moronta',
+    license='MIT',
+)
